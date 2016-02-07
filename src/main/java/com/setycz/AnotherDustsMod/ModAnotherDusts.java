ead99c3ba1b7213bdc6d7d8fcc414580d4c6064c
Jakub Šlesarík <setycz@gmail.com>
2016-02-07 08:37:50 +0100

    first release

--- src/main/java/com/setycz/AnotherDustsMod/ModAnotherDusts.java
+++ src/main/java/com/setycz/AnotherDustsMod/ModAnotherDusts.java
@@ -35,7 +35,7 @@ import net.minecraftforge.oredict.OreDictionary;
 )
 public class ModAnotherDusts {
     public static final String MODID = "anotherdusts";
-    public static final String VERSION = "0.2";
+    public static final String VERSION = "1.0";
     public static final String MODNAME = "Another Dusts";
 
     @Mod.Instance(MODID)
