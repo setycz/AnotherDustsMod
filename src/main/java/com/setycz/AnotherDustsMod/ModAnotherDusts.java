fc2d386dc9f0c8b8f8b0eecbf26eb1b98d0adf68
Jakub Šlesarík <setycz@gmail.com>
2016-02-09 20:27:35 +0100

    version changed to 1.8.9-1.3

--- src/main/java/com/setycz/AnotherDustsMod/ModAnotherDusts.java
+++ src/main/java/com/setycz/AnotherDustsMod/ModAnotherDusts.java
@@ -42,7 +42,7 @@ import java.lang.reflect.Method;
 )
 public class ModAnotherDusts {
     public static final String MODID = "anotherdusts";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
     public static final String MODNAME = "Another Dusts";
     public static final String TCONSTRUCT = "tconstruct";
 
