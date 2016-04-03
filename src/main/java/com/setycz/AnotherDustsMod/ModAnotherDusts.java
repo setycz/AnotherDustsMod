fc0d41eaf320189539cd0c60efe867854e9a5537
Jakub Šlesarík <setycz@gmail.com>
2016-04-03 10:24:29 +0200

    Forge and JEI updated, version changed to 2.0

--- src/main/java/com/setycz/AnotherDustsMod/ModAnotherDusts.java
+++ src/main/java/com/setycz/AnotherDustsMod/ModAnotherDusts.java
@@ -36,11 +36,11 @@ import java.lang.reflect.Method;
         name = ModAnotherDusts.MODNAME,
         version = ModAnotherDusts.VERSION,
         acceptedMinecraftVersions = "1.9",
-        dependencies = "required-after:Forge@[12.16.0.1770,);"
+        dependencies = "required-after:Forge@[12.16.0.1826,);"
 )
 public class ModAnotherDusts {
     public static final String MODID = "anotherdusts";
-    public static final String VERSION = "1.4.1";
+    public static final String VERSION = "2.0";
     public static final String MODNAME = "Another Dusts";
     public static final String TCONSTRUCT = "tconstruct";
 
